bc22c0f1467a9823317daf18badf1d28da62885d
razarevel <razareve@gmail.com>
2024-05-06 13:40:09 +0500

    added fonts

--- hassaPorfolioFrontend/tailwind.config.js
+++ hassaPorfolioFrontend/tailwind.config.js
@@ -1,11 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        Island: "font-island",
+        Light: "Montserrat-Light",
+        Regular: "Montserrat-Regular",
+        Medium: "Montserrat-Medium",
+        Semibold: "Montserrat-SemiBold",
+        Bold: "Montserrat-Bold",
+        Black: "Montserrat-Black",
+      },
+    },
   },
   plugins: [],
-}
+};
