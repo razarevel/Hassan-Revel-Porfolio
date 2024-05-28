37a0eb4c185151fa9bb1bc8ecad5a65a8684d610
razarevel <razareve@gmail.com>
2024-05-28 16:10:41 +0500

    re setting env

--- hassaPorfolioBackend/hassanrevelBackend/asgi.py
+++ hassaPorfolioBackend/hassanrevelBackend/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for hassanPorfolioBackend project.
+ASGI config for hassanrevelBackend project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'hassanPorfolioBackend.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'hassanrevelBackend.settings')
 
 application = get_asgi_application()
--- hassaPorfolioBackend/manage.py
+++ hassaPorfolioBackend/manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'hassanPorfolioBackend.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'hassanrevelBackend.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
