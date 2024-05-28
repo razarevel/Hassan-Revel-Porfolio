ca499d8c74819988bb9081b17e5e6a7d0a5da485
razarevel <razareve@gmail.com>
2024-05-28 16:20:49 +0500

    added two models to simple api solution and blogs

--- hassaPorfolioBackend/simpleApi/models.py
+++ hassaPorfolioBackend/simpleApi/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+
+class solutions(models.Model):
+    img = models.CharField(max_length=255)
+    name = models.CharField(max_length=255)
+
+
+class blogs(models.Model):
+    img = models.CharField(max_length=255)
+    name = models.CharField(max_length=255)
