0c29c0377787bd6d9f456ef97490dc5d2984b735
razarevel <razareve@gmail.com>
2024-05-29 12:15:26 +0500

    adding a link field to blogs

--- hassaPorfolioBackend/simpleApi/models.py
+++ hassaPorfolioBackend/simpleApi/models.py
@@ -11,4 +11,5 @@ class solutions(models.Model):
 class blogs(models.Model):
     img = models.CharField(max_length=255)
     name = models.CharField(max_length=255)
+    link = models.CharField(max_length=255, null=True)
     date = models.DateField(auto_now=True)
