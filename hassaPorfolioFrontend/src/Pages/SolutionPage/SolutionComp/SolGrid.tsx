985d2a35026c77caa034b05bc83bd594abb96bac
razarevel <razareve@gmail.com>
2024-05-30 14:56:12 +0500

    solution page api

--- hassaPorfolioFrontend/src/Pages/SolutionPage/SolutionComp/SolGrid.tsx
+++ hassaPorfolioFrontend/src/Pages/SolutionPage/SolutionComp/SolGrid.tsx
@@ -1,5 +1,6 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
+import { FaChevronLeft } from "react-icons/fa";
 
 interface Solution {
   id: number;
@@ -9,7 +10,7 @@ interface Solution {
 
 export default function SolGrid() {
   const [data, setData] = useState<Solution[]>([]);
-  const count = 1;
+  const [count, setCount] = useState<number>(2);
   const [maxCount, SetMaxCounts] = useState<number>(0);
   const url = "http://127.0.0.1:8000/api/solutions/?page=" + count.toString();
   useEffect(() => {
@@ -45,10 +46,59 @@ export default function SolGrid() {
         ))}
       </div>
       {/* btn */}
-      <div className="flex items-center justify-center w-full mt-[36px]">
+      <div className="px-[16px] sm:px-[30px] lg:px-[5.125vw] my-[56px] flex items-center justify-between lg:max-w-[35vw] mx-auto ">
+        {/* prev */}
+        <div
+          className={
+            "border rounded-full w-[40px] h-[40px] flex items-center justify-center   relative duration-300 " +
+            (count > 1
+              ? " group hover:bg-heavyBlue cursor-pointer"
+              : " opacity-70")
+          }
+          onClick={() => count > 1 && setCount(count - 1)}
+        >
+          <svg
+            fill="#000000"
+            className="w-7 group-hover:fill-white"
+            viewBox="-8.5 0 32 32"
+            version="1.1"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path d="M7.094 15.938l7.688 7.688-3.719 3.563-11.063-11.063 11.313-11.344 3.531 3.5z"></path>
+          </svg>
+        </div>
         {[...Array(maxCount)].map((_, index) => (
-          <p key={index}>{index + 1}</p>
+          <div
+            key={index}
+            className={
+              " rounded-full w-[40px] h-[40px] flex items-center justify-center hover:bg-heavyBlue hover:text-white cursor-pointer " +
+              (index === count - 1 && " bg-heavyBlue text-white")
+            }
+            onClick={() => setCount(index + 1)}
+          >
+            <p>{index + 1}</p>
+          </div>
         ))}
+        {/* next */}
+        <div
+          className={
+            "border rounded-full w-[40px] h-[40px] flex items-center justify-center rotate-180    relative  " +
+            (count < maxCount
+              ? " group hover:bg-heavyBlue cursor-pointer"
+              : " opacity-70")
+          }
+          onClick={() => count < maxCount && setCount(count + 1)}
+        >
+          <svg
+            fill="#000000"
+            className="w-7 group-hover:fill-white"
+            viewBox="-8.5 0 32 32"
+            version="1.1"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path d="M7.094 15.938l7.688 7.688-3.719 3.563-11.063-11.063 11.313-11.344 3.531 3.5z"></path>
+          </svg>
+        </div>
       </div>
     </section>
   );
